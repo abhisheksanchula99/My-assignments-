fb0d85c69fa3d90b4e4bff2a11afb4f857be8b00
abhisheksanchula99 <59160974+abhisheksanchula99@users.noreply.github.com>
2021-08-14 23:46:50 +0530

    Streams API

---
+++ Java_A9_2.java
@@ -0,0 +1,37 @@
+public class Java_A9_Q2
+{
+	int newsId;
+	String postedByUser;
+	String commentByUser;
+	String Comment;
+	public Java_A9_Q2(int newsId, String postedByUser, String commentByUser, String comment) 
+	{
+		super();
+		this.newsId = newsId;
+		this.postedByUser = postedByUser;
+		this.commentByUser = commentByUser;
+		Comment = comment;
+	}
+	public int getNewsId() 
+	{
+		return newsId;
+	}
+	public String getPostedByUser() 
+	{
+		return postedByUser;
+	}
+	public String getCommentByUser() 
+	{
+		return commentByUser;
+	}
+	public String getComment() 
+	{
+		return Comment;
+	}
+	@Override
+	public String toString() 
+	{
+		return "News [newsId=" + newsId + ", postedByUser=" + postedByUser + ", commentByUser=" + commentByUser
+				+ ", Comment=" + Comment + "]";
+	}
+}
---
+++ Java_A9_4.java
@@ -0,0 +1,10 @@
+public class Java_A9_Q4 {
+	String name;
+	String city;
+	public Java_A9_Q4(String name, String city) 
+	{
+		super();
+		this.name = name;
+		this.city = city;
+	}	
+}
