40c4f85f37e44a6fc1634ceaf2ff1d91a8386d7b
abhisheksanchula99 <59160974+abhisheksanchula99@users.noreply.github.com>
2021-08-28 22:26:35 +0530

    Java 11-A10

---
+++ java11_2.java
@@ -0,0 +1,18 @@
+public class java11_2 {
+	
+	var x= 10;
+	
+	public static void main(String[] args) {
+		
+		
+		var<Integer> a = new ArrayList<Integer>();
+		        
+		          
+		var obj = (a, b) -> (a + b);
+		        
+		      
+		          
+		var variable; 
+		      
+	}
+}
---
+++ java11_3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class java11_3 
+{
+	public static void main(String[] args) 
+	{
+		String s= "A quick brown fox jumps over the lazy dog";
+		List<String> list= Arrays.asList(s.split(" "));
+		String[] strArray= list.toArray(String[]::new);
+		System.out.println(Arrays.toString(strArray));
+	}
+}
